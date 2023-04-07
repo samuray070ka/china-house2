d83e366f0647c5925dffbd46e9b6cbc443fb1d58
markelof357 <jasur3355mard@gmail.com>
2023-04-07 21:03:57 +0500

    lorem

--- src/router/contact/Contact.js
+++ src/router/contact/Contact.js
@@ -1,8 +1,98 @@
 import React from 'react'
+import "./Contact.css"
+
+export const MAN__ABOUT = [
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/1.jpg",
+    name: "Shuhratovna Iroda",
+    job: "Sotish bo'yicha  menejer",
+    num: "97 115 88 88",
+
+  },
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/2.jpg",
+    name: "Oxunova Nozidil",
+    job: "Sotish bo'yicha  menejer",
+    num: "97 125 88 88",
+
+  },
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/3.jpg",
+    name: "Maxamadievna Shaxnoza",
+    job: "Sotish bo'yicha  menejer",
+    num: "97 175 88 88",
+
+  },
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/4.jpg",
+    name: "Mashrabbayeva Mahliyo",
+    job: "Sotish bo'yicha  menejer",
+    num: "",
+
+  },
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/6.jpg",
+    name: "Normurodova Madinabonu",
+    job: "Sotish bo'yicha  menejer",
+    num: "99 924 88 88",
+    
+  },
+  {
+    img: "https://china-house.uz/wp-content/themes/china-house/images/sales/7.jpg",
+    name: "Ismailova Muslima",
+    job: "Sotish bo'yicha  menejer",
+    num: "99 926 88 88",
+
+  },
+]
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="container">
+      <div className="contact">
+        
+        <div className="con__sidebar">
+
+          <img className='csidebar__logo' src="https://china-house.uz/wp-content/themes/china-house/images/sales/logo-shoxsaroy-white2.svg" alt="" />
+          <img className='csidebar__loc' src="https://china-house.uz/wp-content/themes/china-house/images/sales/shohsaroy.jpg" alt="" />
+
+          <br />
+          <h4>Посмотреть на карте</h4>
+
+          <img className='csidebar__logo' src="https://china-house.uz/wp-content/themes/china-house/images/sales/logo-shanghai-white2.svg" alt="" />
+
+          <img className='csidebar__loc' src="https://china-house.uz/wp-content/themes/china-house/images/sales/shanghai.jpg" alt="" />
+          <h4>Посмотреть на карте</h4>
+        </div>
+
+
+        <div className="con__about">
+          {
+            MAN__ABOUT?.map((item, inx)=><div key={inx} className="cabout__card">
+              <img className="ccard__img" src={item.img} alt="" />
+
+              <h3 className='ccard__name'>
+                {item.name}
+              </h3>
+
+              <h4 className='ccard__job'>
+                {item.job}
+              </h4>
+
+              <button className='ccard__btn'>
+                Xabar yozish uchun
+              </button>
+
+            <h4 className='ccard__num'>+998 <span>{item.num}</span></h4>
+            </div>)
+          }
+
+
+
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
