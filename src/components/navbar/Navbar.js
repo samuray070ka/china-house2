b59810704c7e269aa01b9c4713e0fc5fe82e2568
islomjon1202 <izlomjonkhon777@gmail.com>
2023-04-08 08:09:14 +0500

    dfg

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Navbar.css"
+import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <Link to="/">
+        <p>Asosiy</p>
+      </Link>
+      <Link to="/plan">
+        <p>Rejalashtirish</p>
+      </Link>
+      <Link to="/contact">
+        <p>To'lov rejasi</p>
+      </Link>
+      <Link to="/ipoteka">
+        <p>Ipoteka</p>
+      </Link>
+    </nav>
   )
 }
 
