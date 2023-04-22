55d962e8b809c8058711eb12f311bef18c6d6426
markelof357 <jasur3355mard@gmail.com>
2023-04-22 16:49:54 +0500

    lorem

--- src/router/admin/create/Create.js
+++ src/router/admin/create/Create.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Create.css";
 
 function Create() {
   return (
-      <div >
-       <h2>Create</h2>
+    <div className="create">
+      <div className="create__card">
+        <input className="create__inp" type="text" placeholder="Video Url" />
+        <button className="create__btn">Create</button>
       </div>
-  )
+    </div>
+  );
 }
-export default Create
+export default Create;
